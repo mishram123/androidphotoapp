9dde6e4f8592cac8318ff46010a3b0c3c27a6932
Mannan Mishra <mm3033@scarletmail.rutgers.edu>
2023-04-26 23:33:34 -0400

    create album, delete album, rename album work

--- app/src/main/java/com/example/androidphotolibrary/controller/UserSystemController.java
+++ app/src/main/java/com/example/androidphotolibrary/controller/UserSystemController.java
@@ -1,12 +1,19 @@
 package com.example.androidphotolibrary.controller;
 
+import com.example.androidphotolibrary.model.*;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidphotolibrary.R;
@@ -19,6 +26,14 @@ public class UserSystemController extends AppCompatActivity{
     private TextView selectedAlbumTextView;
     private Button createAlbumButton, deleteAlbumButton, renameAlbumButton, openAlbumButton, searchPhotosButton;
     private ArrayList<String> albumsList;
+
+    private String selectedAlbum;
+
+    public String getSelectedAlbum(){
+        return selectedAlbum;
+    }
+
+    User mainUser = new User("user");
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -37,31 +52,105 @@ public class UserSystemController extends AppCompatActivity{
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, albumsList);
         albumListView.setAdapter(adapter);
 
+
         albumListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = albumsList.get(position);
-                selectedAlbumTextView.setText("Selected Item: " + selectedItem);
+                selectedAlbum = albumsList.get(position);
+                selectedAlbumTextView.setText("Selected Item: " + selectedAlbum);
+
             }
         });
+
 
         createAlbumButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(UserSystemController.this);
+                builder.setTitle("Enter album name:");
 
+                final EditText input = new EditText(UserSystemController.this);
+
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String albumName = input.getText().toString();
+                        Album newAlbum = new Album(albumName);
+                        mainUser.addAlbum(newAlbum);
+                        albumsList.add(albumName);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
             }
         });
 
         deleteAlbumButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                String deletingAlbum = getSelectedAlbum();
 
+                if(deletingAlbum != null){
+                    for(int i = 0; i<mainUser.getAlbums().size(); i++){
+                        if(deletingAlbum.compareTo(mainUser.getAlbums().get(i).getName()) == 0){
+                            mainUser.getAlbums().remove(i);
+                        }
+                    }
+                    albumsList.remove(deletingAlbum);
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
 
         renameAlbumButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(UserSystemController.this);
+                builder.setTitle("Enter album name:");
+
+                final EditText input = new EditText(UserSystemController.this);
+
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                        String albumName = input.getText().toString();
+                        for(int i = 0; i<mainUser.getAlbums().size(); i++){
+                            if(albumName.compareTo(mainUser.getAlbums().get(i).getName())==0){
+                                mainUser.getAlbums().get(i).setName(albumName);
+                                break;
+                            }
+                        }
+                        String deletingAlbum = getSelectedAlbum();
+                        albumsList.remove(deletingAlbum);
+                        albumsList.add(albumName);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
 
             }
         });
